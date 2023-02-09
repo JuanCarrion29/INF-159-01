bd3049c1fe9262e7f78a70d57f5b52af72cd18b3
jc21-1064@unphu.edu.do <jc21-1064@unphu.edu.do>
2023-02-09 17:37:40 -0400

    first commit

---
+++ 1.fibonnaci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number of terms: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int firstTerm = 0;
+            int secondTerm = 1;
+            int nextTerm;
+
+            Console.WriteLine("Fibonacci series: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= 1)
+                    nextTerm = i;
+                else
+                {
+                    nextTerm = firstTerm + secondTerm;
+                    firstTerm = secondTerm;
+                    secondTerm = nextTerm;
+                }
+
+                Console.WriteLine(nextTerm);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+    
+
+
+
